9018716fe6d9093d4a5908907a33b61243a689a1
eriko-mahalo0917 <erikofuruichi11210812@gmail.com>
2025-11-16 19:06:13 +0900

    相対パスでファイルを呼び出す

--- google_apis/sheets_reader.py
+++ google_apis/sheets_reader.py
@@ -1,23 +1,54 @@
 #=========================================================
 #インポート
 import gspread
+import os
+from dotenv import load_dotenv #直接呼び出す
 
 #JSONファイルを読み込みするためのモジュール
 from google.oauth2.service_account import Credentials
+#logger
+from utils import logger
+#Pathを使うため
+from pathlib import Path
+
+#.envから設定を読み込むための
+#from utils.env_loader import config　#直接呼ぶからコメントアウト
 #=========================================================
 
-#1つ目のフロー
-#JSONファイルの指定
-JSON_FILE = "creds.json"
-
-#スプレッドシートのID
-SPREADSHEET_ID ="1PrESjDHuqNpsZfo-fvd6hb8tOuAXl63aDio7hdjt6hg"
-
-
-#２つ目のフロー
-#承認&実行
-#リスト：実行していいよの定型文みいなもの
-scopes = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
-]
+class SheetReader:
+    def __init__(self):
+        #utils/logger.py のロガー（SimpleLogger）をセットアップする
+        self.logger_setup = logger.SimpleLogger()
+        self.logger = self.logger_setup.get_logger()
+        
+        #１つ目のフロー
+        #.envの場所を特定　Path(__file__)はファイル自身の住所！
+        self.env_path = Path(__file__).parents[1]
+        dotenv_path = self.env_path/".env"
+        load_dotenv(dotenv_path)
+        
+        #承認&実行
+        #リスト：実行していいよの定型文みたいなもの
+        scopes = [
+            'https://www.googleapis.com/auth/spreadsheets',
+            'https://www.googleapis.com/auth/drive'
+        ]
+        
+        #２つ目のフロー
+        #開きたいスプレッドシートのIDを取得する
+        try:
+            self.logger.info("SPREADSHEET_IDを取得します")
+            #jsonファイルは一時的に保存？ちょっとよく分からない
+            json_path_from_env = os.getenv("SERVICE_ACCOUNT_FILE_PATH")
+            self.spreadsheet_id = os.getenv("SPREADSHEET_ID")
+            
+            
+        
+        
+        
+        
+        #３つ目のフロー
+        #スプレッドシートを読み取る
+        
+        
+        
