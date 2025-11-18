e18f6e95261cf999c822f012d88f881b410b9278
eriko-mahalo0917 <erikofuruichi11210812@gmail.com>
2025-11-19 00:30:55 +0900

    現時点でのコード&日本語での構成を追加しました。

--- google_apis/sheets_reader.py
+++ google_apis/sheets_reader.py
@@ -21,11 +21,22 @@ class SheetReader:
         self.logger_setup = logger.SimpleLogger()
         self.logger = self.logger_setup.get_logger()
         
-        #１つ目のフロー
-        #.envの場所を特定　Path(__file__)はファイル自身の住所！
+    #１つ目のフロー
+    #WSとのAPI連携
+    ##.envの場所を特定　Path(__file__)はファイル自身の住所！
+    def gspread_api(self):
         self.env_path = Path(__file__).parents[1]
         dotenv_path = self.env_path/".env"
         load_dotenv(dotenv_path)
+        
+        json_path_from_env = os.getenv("SERVICE_ACCOUNT_FILE_PATH")
+        self.spreadsheet_id = os.getenv("SPREADSHEET_ID")
+        
+        
+        #ここでID取得のときにエラーがでないかを確認する！
+        #まだ未作成
+        
+        
         
         #承認&実行
         #リスト：実行していいよの定型文みたいなもの
@@ -34,21 +45,15 @@ class SheetReader:
             'https://www.googleapis.com/auth/drive'
         ]
         
-        #２つ目のフロー
-        #開きたいスプレッドシートのIDを取得する
-        try:
-            self.logger.info("SPREADSHEET_IDを取得します")
-            #jsonファイルは一時的に保存？ちょっとよく分からない
-            json_path_from_env = os.getenv("SERVICE_ACCOUNT_FILE_PATH")
-            self.spreadsheet_id = os.getenv("SPREADSHEET_ID")
-            
-            
-        
-        
-        
-        
-        #３つ目のフロー
-        #スプレッドシートを読み取る
-        
-        
+    #２つ目のフロー
+    #スプレッドシートのクリニック一覧シートのデータを取得
+    #取得した歯医者さんの名前を全てDataFrameとして読み込む
+    
+    
+    
+    
+    #３つ目のフロー
+    #取得したDataFrameを受け取る
+    #DataFrameをチェックし、入力済みのステータスがない行だけを抽出
+    #抽出したデータから、クリニック名のリストを返す
         
