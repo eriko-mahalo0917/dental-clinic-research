5cfe75602c15c820b900a38782cb39eece378226
eriko-mahalo0917 <erikofuruichi11210812@gmail.com>
2026-02-09 00:15:30 +0900

    configファイルを追加

---
+++ config.py
@@ -0,0 +1,6 @@
+TEST_URL = "https://docs.google.com/spreadsheets/d/1PrESjDHuqNpsZfo-fvd6hb8tOuAXl63aDio7hdjt6hg/edit?gid=1025571184#gid=1025571184"
+TEST_SHEET = "テスト一覧"
+CLINIC_KEY = "クリニック名"
+STATUS_KEY = "ステータス"
+
+SPREADSHEET_ID = "1PrESjDHuqNpsZfo-fvd6hb8tOuAXl63aDio7hdjt6hg"
