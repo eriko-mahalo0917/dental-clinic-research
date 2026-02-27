87305f4a0db0d1e98a533f93912bf14bf114fff1
eriko-mahalo0917 <erikofuruichi11210812@gmail.com>
2026-02-28 00:20:40 +0900

    READMEの一部修正とconfigを修正

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ SPREADSHEET_ID="1PrESjDHuqNpsZfo-fvd6hb8tOuAXl63aDio7hdjt6hg"
 #=================================================
 #シート内の設定(固定値)
 #=================================================
-CLINIC_SHEET = "テスト一覧"
+CLINIC_SHEET = "クリニック一覧"
 CLINIC_KEY = "クリニック名"
 STATUS_KEY = "ステータス"
